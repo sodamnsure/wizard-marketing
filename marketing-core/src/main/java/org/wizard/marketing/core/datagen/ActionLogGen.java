4d090ad9dcadabf2aac85fa0b370c73b060c7f6a
sodamnsure <zhaiguoweii5@163.com>
2021-11-04 17:26:45 +0800

    修改为原来测试服务器地址

--- marketing-core/src/main/java/org/wizard/marketing/core/datagen/ActionLogGen.java
+++ marketing-core/src/main/java/org/wizard/marketing/core/datagen/ActionLogGen.java
@@ -30,7 +30,7 @@ public class ActionLogGen {
                     ObjectMapper mapper = new ObjectMapper();
                     // 配置kafka
                     Properties props = new Properties();
-                    props.setProperty("bootstrap.servers", "client:9094");
+                    props.setProperty("bootstrap.servers", "feelings:9092");
                     props.put("acks", "all");
                     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
                     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
