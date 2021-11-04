4585e2a4cabd464c4890cdf77e6f4195ff7278c2
sodamnsure <zhaiguoweii5@163.com>
2021-11-04 19:56:22 +0800

    工具类部分迁移

---
+++ marketing-core/src/main/java/org/wizard/marketing/engine/utils/ConnectionUtils.java
@@ -0,0 +1,70 @@
+package org.wizard.marketing.engine.utils;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.wizard.marketing.core.common.constant.LoadConstant;
+import redis.clients.jedis.Jedis;
+
+import java.io.IOException;
+import java.sql.DriverManager;
+
+/**
+ * @Author: sodamnsure
+ * @Date: 2021/8/26 7:57 下午
+ * @Desc: 客户端连接创建工具
+ */
+@Slf4j
+public class ConnectionUtils {
+    static Config config = ConfigFactory.load();
+
+    /**
+     * 获取HBASE连接
+     *
+     * @return 返回HBASE连接
+     */
+    public static Connection getHbaseConnection() throws IOException {
+        log.debug("HBASE连接准备创建...........");
+        // 创建HBASE配置
+        Configuration conf = HBaseConfiguration.create();
+        conf.set("hbase.zookeeper.quorum", config.getString(LoadConstant.HBASE_ZK_QUORUM));
+        // 创建HBASE连接
+        Connection conn = ConnectionFactory.createConnection(conf);
+        log.debug("创建HBASE连接成功...........");
+        return conn;
+    }
+
+    /**
+     * 获取ClickHouse连接
+     *
+     * @return 返回ClickHouse连接
+     */
+    public static java.sql.Connection getClickHouseConnection() throws Exception {
+        log.debug("ClickHouse连接准备创建...........");
+        String ckDriver = config.getString(LoadConstant.CK_JDBC_DRIVER);
+        String ckUrl = config.getString(LoadConstant.CK_JDBC_URL);
+
+        Class.forName(ckDriver);
+        java.sql.Connection conn = DriverManager.getConnection(ckUrl);
+        log.debug("创建ClickHouse连接成功...........");
+        return conn;
+    }
+
+    public static Jedis getRedisConnection() {
+        String host = config.getString(ConfigNames.REDIS_HOST);
+        int port = config.getInt(ConfigNames.REDIS_PORT);
+        Jedis jedis = new Jedis(host, port);
+        String ping = jedis.ping();
+        if (StringUtils.isNoneBlank(ping)) {
+            log.debug("创建Redis连接成功...........");
+        } else {
+            log.error("创建Redis连接失败...........");
+        }
+        return jedis;
+    }
+}
---
+++ marketing-core/src/main/java/org/wizard/marketing/engine/utils/StateDescContainer.java
@@ -0,0 +1,28 @@
+package org.wizard.marketing.engine.utils;
+
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.state.StateTtlConfig;
+import org.apache.flink.api.common.time.Time;
+import org.wizard.marketing.engine.beans.EventBean;
+
+/**
+ * @Author: sodamnsure
+ * @Date: 2021/9/17 11:14 上午
+ * @Desc: 状态统一管理类
+ */
+public class StateDescContainer {
+    // 默认state存储时间
+    private static final Integer TTL_TIME = 2;
+
+    /**
+     * 近期行为事件存储状态描述
+     */
+    public static ListStateDescriptor<EventBean> getEventBeansDesc() {
+        ListStateDescriptor<EventBean> eventBeansDesc = new ListStateDescriptor<>("event_beans", EventBean.class);
+        StateTtlConfig stateTtlConfig = StateTtlConfig.newBuilder(Time.hours(TTL_TIME)).build();
+        eventBeansDesc.enableTimeToLive(stateTtlConfig);
+
+        return eventBeansDesc;
+    }
+
+}
