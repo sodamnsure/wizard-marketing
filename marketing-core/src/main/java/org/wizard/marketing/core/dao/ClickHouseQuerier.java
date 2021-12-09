4d38f9e0678bd526256df9a7b7ed7510075c35c3
sodamnsure <zhaiguoweii5@163.com>
2021-12-09 14:41:12 +0800

    添加更新缓存和写入缓存的功能

--- marketing-core/src/main/java/org/wizard/marketing/core/dao/ClickHouseQuerier.java
+++ marketing-core/src/main/java/org/wizard/marketing/core/dao/ClickHouseQuerier.java
@@ -14,6 +14,7 @@ import org.wizard.marketing.core.utils.EventUtils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -102,6 +103,8 @@ public class ClickHouseQuerier {
         BufferData bufferData = bufferManager.getDataFromBuffer(bufferKey);
         Map<String, String> valueMap = bufferData.getValueMap();
         Set<String> keySet = valueMap.keySet();
+
+        long current = System.currentTimeMillis();
         for (String key : keySet) {
             String[] split = key.split(":");
             long bufferStartTime = Long.parseLong(split[0]);
@@ -117,6 +120,13 @@ public class ClickHouseQuerier {
 
             // 查询范围和缓存范围完全相同，直接返回缓存结果
             if (bufferStartTime == queryRangeStart && bufferEndTime == queryRangeEnd) {
+                // 将原缓存结果删除
+                bufferManager.deleteBufferKey(bufferKey, key);
+
+                HashMap<String, String> putMap = new HashMap<>();
+                putMap.put(bufferStartTime + ":" + bufferEndTime + ":" + current, bufferSeqStr);
+                bufferManager.putDataToBuffer(bufferKey, putMap);
+
                 return EventUtils.eventSeqStrMatchRegexCount(bufferSeqStr, combCondition.getMatchPattern());
             }
 
@@ -128,8 +138,19 @@ public class ClickHouseQuerier {
                     return bufferCount;
                 } else {
                     // 调整查询时间，去ClickHouse中查询一小段
-                    String eventSeqStr = getCombConditionStr(deviceId, combCondition, bufferEndTime, queryRangeEnd);
-                    return EventUtils.eventSeqStrMatchRegexCount(bufferSeqStr + eventSeqStr, combCondition.getMatchPattern());
+                    String rightSeqStr = getCombConditionStr(deviceId, combCondition, bufferEndTime, queryRangeEnd);
+                    // 将原缓存结果删除
+                    bufferManager.deleteBufferKey(bufferKey, key);
+
+                    // 将结果写入缓存，更新插入时间
+                    HashMap<String, String> putMap = new HashMap<>();
+                    // 放入之前的缓存数据，三种类型：原buffer区间，右分段区间，原buffer区间+右分段区间
+                    putMap.put(bufferStartTime + ":" + bufferEndTime + ":" + current, bufferSeqStr);
+                    putMap.put(bufferEndTime + ":" + queryRangeEnd + ":" + current, rightSeqStr);
+                    putMap.put(bufferStartTime + ":" + queryRangeEnd + ":" + current, bufferSeqStr + rightSeqStr);
+                    bufferManager.putDataToBuffer(bufferKey, putMap);
+
+                    return EventUtils.eventSeqStrMatchRegexCount(bufferSeqStr + rightSeqStr, combCondition.getMatchPattern());
                 }
             }
 
@@ -141,8 +162,20 @@ public class ClickHouseQuerier {
                     return bufferCount;
                 } else {
                     // 调整查询时间，去ClickHouse中查询一小段
-                    String eventSeqStr = getCombConditionStr(deviceId, combCondition, queryRangeStart, bufferStartTime);
-                    return EventUtils.eventSeqStrMatchRegexCount(eventSeqStr + bufferSeqStr, combCondition.getMatchPattern());
+                    String leftSeqStr = getCombConditionStr(deviceId, combCondition, queryRangeStart, bufferStartTime);
+
+                    // 将原缓存结果删除
+                    bufferManager.deleteBufferKey(bufferKey, key);
+
+                    // 将结果写入缓存，更新插入时间
+                    HashMap<String, String> putMap = new HashMap<>();
+                    // 放入之前的缓存数据，三种类型：原buffer区间，右分段区间，原buffer区间+右分段区间
+                    putMap.put(bufferStartTime + ":" + bufferEndTime + ":" + current, bufferSeqStr);
+                    putMap.put(queryRangeStart + ":" + bufferStartTime + ":" + current, leftSeqStr);
+                    putMap.put(queryRangeStart + ":" + queryRangeEnd + ":" + current, leftSeqStr + bufferSeqStr);
+                    bufferManager.putDataToBuffer(bufferKey, putMap);
+
+                    return EventUtils.eventSeqStrMatchRegexCount(leftSeqStr + bufferSeqStr, combCondition.getMatchPattern());
                 }
             }
 
@@ -151,6 +184,15 @@ public class ClickHouseQuerier {
                 int bufferCount = EventUtils.eventSeqStrMatchRegexCount(bufferSeqStr, combCondition.getMatchPattern());
                 int queryMinCount = combCondition.getMinLimit();
                 if (bufferCount >= queryMinCount) {
+                    // 将原缓存结果删除
+                    bufferManager.deleteBufferKey(bufferKey, key);
+
+                    // 将结果写入缓存，更新插入时间
+                    HashMap<String, String> putMap = new HashMap<>();
+                    // 放入之前的缓存数据，三种类型：原buffer区间，右分段区间，原buffer区间+右分段区间
+                    putMap.put(bufferStartTime + ":" + bufferEndTime + ":" + current, bufferSeqStr);
+                    bufferManager.putDataToBuffer(bufferKey, putMap);
+
                     return bufferCount;
                 }
             }
@@ -158,10 +200,13 @@ public class ClickHouseQuerier {
 
         // 先查询到用户在组合条件中做过的事件的字符串序列
         String eventSeqStr = getCombConditionStr(deviceId, combCondition, queryRangeStart, queryRangeEnd);
+        // 将查询结果写入缓存
+        HashMap<String, String> putMap = new HashMap<>();
+        putMap.put(queryRangeStart + ":" + queryRangeEnd + ":" + current, eventSeqStr);
+        bufferManager.putDataToBuffer(bufferKey, putMap);
+
         // 调用工具，来获取事件序列与正则表达式的匹配次数--即组合条件发生的次数
         int count = EventUtils.eventSeqStrMatchRegexCount(eventSeqStr, combCondition.getMatchPattern());
-
-        // TODO 缓存写处理
 
         log.debug("在ClickHouse中查询组合事件条件，得到的事件序列字符串: {}, 正则表达式: {}, 匹配结果: {}", eventSeqStr, combCondition.getMatchPattern(), count);
         return count;
