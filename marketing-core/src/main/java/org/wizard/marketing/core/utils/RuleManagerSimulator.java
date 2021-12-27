f28fde6a333240a8f6c34bd9260204f5f539718a
sodamnsure <zhaiguoweii5@163.com>
2021-12-27 15:12:42 +0800

    规则管理平台(Web 系统)模拟器程序开发1.0

---
+++ marketing-core/src/main/java/org/wizard/marketing/core/utils/RuleManagerSimulator.java
@@ -0,0 +1,19 @@
+package org.wizard.marketing.core.utils;
+
+/**
+ * @Author: sodamnsure
+ * @Date: 2021/12/27 3:02 PM
+ * @Desc: 规则管理平台(Web 系统)模拟器
+ */
+public class RuleManagerSimulator {
+    // 插入新规则到MySQL
+
+
+    // 更新已存在的规则
+
+    // 删除规则
+
+    // 启用规则
+
+    // 停用规则
+}
